11da1b2034a0adddb6b9a6ad6d76a0235ea6257d
Deus <amart362@students.kennesaw.edu>
2019-01-18 17:39:07 -0500

    Intial add to repo

---
+++ KSU_CSE1322/Mod 1 lab1b.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amadeus_Martinez_Mod1_Lab1B_section01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[9] { 1, 4, 13, 43, -25, 17, 22, -37, 29 };
+            Console.WriteLine(nums.findLargest());
+        }
+
+        public static int findLargest(int[] array)
+        {
+
+            int index = 0;
+            int maxInt = Int32.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int value = array[i];
+                if (value > maxInt)
+                {
+                    maxInt = value;
+                    index = i;
+
+                    
+                }
+                return maxInt;
+            }
+            
+    }
+}
